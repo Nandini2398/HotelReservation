462aeb2a82b97e4f3060efd34f126537977ec3ad
Nandini Yarra <yarranandhini1416@gmail.com>
2022-01-13 07:19:33 +0530

    UPDATED:to find the cheapest hotel

--- src/main/java/com/bridgelabz/Hotel_Reservation/HotelReservationMain.java
+++ src/main/java/com/bridgelabz/Hotel_Reservation/HotelReservationMain.java
@@ -1,12 +1,7 @@
 package com.bridgelabz.Hotel_Reservation;
 
 public class HotelReservationMain {
-
-		public static void main(String[] args) {
-			System.out.println(" Welcome To Hotel Reservation");
-			HotelReservationInterface hotelReservation = new HotelReservation();
-			hotelReservation.addHotel();
-			hotelReservation.printHotelList();
-		}
+	public static void main(String[] args) {
+		System.out.println(" Welcome To Hotel Reservation");
 	}
-
+}
