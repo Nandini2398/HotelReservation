e19cf4d7320167a702f655a1cb701cb843b0193b
Nandini Yarra <yarranandhini1416@gmail.com>
2022-01-13 07:29:19 +0530

    UPDATED:weekend and weekday costs

--- src/main/java/com/bridgelabz/Hotel_Reservation/HotelReservation.java
+++ src/main/java/com/bridgelabz/Hotel_Reservation/HotelReservation.java
@@ -6,35 +6,24 @@ import java.util.Comparator;
 import java.util.Optional;
 
 public class HotelReservation implements HotelReservationInterface {
-
 	ArrayList<Hotel> hotelList = new ArrayList<Hotel>();
-	Hotel hotel;
-
-	public void addHotel(String hotelName, int rating, double CustomerRate) {
-		
+	Hotel hotel ;
+	public void addHotel(String hotelName, int rating, double weekdayRate, double weekndRate) {
 		hotel = new Hotel();
 		hotel.setHotelName(hotelName);
 		hotel.setRating(rating);
-		hotel.setCustomerCost(CustomerRate);
+		hotel.setWeekDayRate(weekdayRate);
+		hotel.setWeekendRate(weekndRate);
 		hotelList.add(hotel);
 	}
-	
 	public int getHotelListSize() {
 		return hotelList.size();
 	}
-	
 	public void printHotelList() {
 		System.out.println(hotelList);
 	}
-	
 	public ArrayList<Hotel> getHotelList(){
 		return hotelList;
-	}
-
-	public Hotel getCheapestHotel(LocalDate startDate, LocalDate endDate) {
-
-		Optional<Hotel> resultList = hotelList.stream().min(Comparator.comparing(Hotel::getRegularCustomerCost));
-		return resultList.get();
 	}
 
 }
