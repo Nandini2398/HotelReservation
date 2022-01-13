8824fded687c0f9d2ddfc202b60ad66cb4302cc5
Nandini Yarra <yarranandhini1416@gmail.com>
2022-01-13 07:29:35 +0530

    UPDATED:weekend and weekday costs

--- src/main/java/com/bridgelabz/Hotel_Reservation/HotelReservationInterface.java
+++ src/main/java/com/bridgelabz/Hotel_Reservation/HotelReservationInterface.java
@@ -2,8 +2,8 @@ package com.bridgelabz.Hotel_Reservation;
 import java.util.ArrayList;
 
 public interface HotelReservationInterface {
-			public void addHotel(String hotelName,int rating, double regularCustomerRate);
-			public int getHotelListSize();
-			public void printHotelList();
-			public ArrayList<Hotel> getHotelList();
+	public void addHotel(String hotelName,int rating, double weekDayRate,double weekendRate);
+	public int getHotelListSize();
+	public void printHotelList();
+	public ArrayList<Hotel> getHotelList();
 }
