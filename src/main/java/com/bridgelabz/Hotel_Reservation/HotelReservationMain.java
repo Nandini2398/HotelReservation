7668ced323a2d9fd2b7a63f97d1823ee96540c95
Nandini Yarra <yarranandhini1416@gmail.com>
2022-01-14 20:25:05 +0530

    UPDATED:finding a best rated hotel

--- src/main/java/com/bridgelabz/Hotel_Reservation/HotelReservationMain.java
+++ src/main/java/com/bridgelabz/Hotel_Reservation/HotelReservationMain.java
@@ -2,15 +2,20 @@ package com.bridgelabz.Hotel_Reservation;
 import java.time.LocalDate;
 import java.time.Month;
 
+import java.time.LocalDate;
+import java.time.Month;
+
 public class HotelReservationMain {
-	System.out.println(" Welcome To Hotel Reservation");
-	HotelReservationInterface hotelReservation = new HotelReservation();
-	hotelReservation.addHotel("Lakewood", 3, 110,90);
-	hotelReservation.addHotel("Bridgewood", 4, 150,50);
-	hotelReservation.addHotel("Ridgewood", 5, 220,150);
-	hotelReservation.printHotelList();
-	LocalDate startDate = LocalDate.of(2021, Month.SEPTEMBER, 10);    
-	LocalDate endDate = LocalDate.of(2021, Month.SEPTEMBER, 11);    
-	hotelReservation.getCheapestHotel(startDate, endDate);
-  }
+	public static void main(String[] args) {
+		System.out.println(" Welcome To Hotel Reservation");
+		HotelReservationInterface hotelReservation = new HotelReservation();
+		hotelReservation.addHotel("Lakewood", 3, 110,90);
+		hotelReservation.addHotel("Bridgewood", 4, 150,50);
+		hotelReservation.addHotel("Ridgewood", 5, 220,150);
+		hotelReservation.printHotelList();
+		LocalDate startDate = LocalDate.of(2021, Month.SEPTEMBER, 10);    
+		LocalDate endDate = LocalDate.of(2021, Month.SEPTEMBER, 11);    
+		hotelReservation.getCheapestHotel(startDate, endDate);
+		hotelReservation.getBestRatedHotel(startDate, endDate);
+	}
 }
